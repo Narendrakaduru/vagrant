5dbe5462fb79dd89cdf8edd2fd3aa1d4a88947b0
nani <kadurunarendra@gmail.com>
2023-11-13 12:01:54 +0530

    vagrant

---
+++ Vagrantfile
@@ -0,0 +1,35 @@
+Vagrant.configure("2") do |config|
+  config.vm.define "master" do |master|
+    master.vm.box = "bento/ubuntu-22.04"
+    master.vm.network "private_network", type: "static", ip: "192.168.10.120"
+    master.vm.hostname = "vagrant-master"
+    master.vm.provider "virtualbox" do |vb|
+      vb.name = "Vagrant-Master"
+      vb.memory = 2048
+      vb.cpus = 2
+    end
+    config.vm.provision "shell", path: "scripts/master.sh"
+  end
+
+  num_nodes = 1
+  ip_address = 121
+
+  num_nodes.times do |i|
+    node_name = "node-%02d" % (i + 1)
+    node_ip = "192.168.10.#{ip_address}"
+    
+    config.vm.define node_name do |node|
+      node.vm.box = "bento/ubuntu-22.04"
+      node.vm.network "private_network", type: "static", ip: node_ip
+      node.vm.hostname = node_name
+      node.vm.provider "virtualbox" do |vb|
+        vb.name = "Vagrant-#{node_name}"
+        vb.memory = 2048
+        vb.cpus = 2
+      end
+      config.vm.provision "shell", path: "scripts/node.sh"
+    end
+
+    ip_address += 1
+  end
+end
---
+++ scripts/node.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update the package list and install OpenJDK 17
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk wget curl
+
+echo "Download maven from central repo"
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz
+sudo tar -xvzf apache-maven*.tar.gz
+sudo rm -rf apache-maven*.tar.gz
+sudo mv apache-maven* /opt/maven
+
+# Update path in bashrc
+echo "Update path in bashrc"
+echo 'JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+M2_HOME=/opt/maven
+PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$M2_HOME/bin
+
+export JAVA_HOME
+export M2_HOME
+export PATH' | sudo tee -a /root/.bashrc
+sudo source /root/.bashrc
+
+
+# Add user to Sudoers file
+echo "Add user to Sudoers file"
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+
+# Enable Password Authentication
+echo "Enable Password Authentication"
+sudo echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo chmod +x install-docker.sh
+./install-docker.sh
